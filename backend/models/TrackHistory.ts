f94c98533aedd99111c4fbc2a012fabebbe27393
Sanzhar Beishekeev <franki-55@mail.ru>
2025-01-09 14:31:24 +0600

    HomeWork-83 Создал модель для истории прослушивания трека, дату прослушивания сделал автоматическую через анонимную функцию

---
+++ backend/models/TrackHistory.ts
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const TrackHistorySchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    track: { type: Schema.Types.ObjectId,
+        ref: 'Track',
+        required: true
+    },
+    datetime: {
+        type: String,
+        default: () => new Date().toISOString(),
+        required: true
+    }
+});
+
+const TrackHistory = mongoose.model('TrackHistory', TrackHistorySchema);
+
+export default TrackHistory;
