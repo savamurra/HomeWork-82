2617b7e95950b3ae2cdb33ccfd936f5ebfd1965b
Sanzhar Beishekeev <franki-55@mail.ru>
2025-01-15 16:59:51 +0600

    HomeWork-85 [frontend] init

---
+++ frontend/src/main.tsx
@@ -0,0 +1,14 @@
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import {store} from "../app/store.ts";
+
+createRoot(document.getElementById("root")!).render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
+);
